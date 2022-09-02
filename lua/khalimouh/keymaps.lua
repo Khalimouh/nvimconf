180e358de0e683f0e086722f69e463b38b7169f6
Mohamed Ramdane DEBIANE <M.debiane71@gmail.com>
2022-09-02 23:57:51 +0200

    DB keymap

--- lua/khalimouh/keymaps.lua
+++ lua/khalimouh/keymaps.lua
@@ -26,7 +26,7 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<C-p>", ":Telescope find_files <CR>", opts)
-keymap("n", "<C-^>", ":Telescope <CR>", opts)
+keymap("n", "<C-a>", ":DBUIToggle <CR>", opts)
 
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
