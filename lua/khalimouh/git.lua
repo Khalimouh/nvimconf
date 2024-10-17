74786a451ab71b16fb1f8910020d37be96b7a755
Mohamed-Ramdane DEBIANE <mohamed-ramdane.debiane@stack-labs.com>
2024-10-17 14:23:48 +0200

    fix: giitsigns

--- lua/khalimouh/git.lua
+++ lua/khalimouh/git.lua
@@ -1,9 +1,10 @@
 local status_ok, gitsigns = pcall(require, "gitsigns")
+
 if not status_ok then
     return
 end
 
-require('gitsigns').setup {
+gitsigns.setup {
     signs                        = {
         add          = { text = '│' },
         change       = { text = '│' },
