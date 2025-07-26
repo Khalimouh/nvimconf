0d4dc33e09a8ac5ab74cb354b7844ca5151542a1
Mohamed Ramdane DEBIANE <m.debiane71@gmail.com>
2025-07-26 09:22:33 +0200

    Fix none ls with pylint

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,24 @@
 local config = require "nvchad.configs.lspconfig"
 
-local on_attach = config.on_attach
 local capabilities = config.capabilities
+local on_attach = function(client, bufnr)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "gi", require("telescope.builtin").lsp_implementations, bufopts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set("n", "<space>D", require("telescope.builtin").lsp_type_definitions, bufopts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, bufopts)
+    vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
+end
 
 local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
@@ -9,7 +26,7 @@ local util = require "lspconfig/util"
 lspconfig.pyright.setup {
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {"python"},
+    filetypes = { "python" },
 }
 
 local servers = { "bashls", "lua_ls", "terraformls", "jqls", "tflint" }
