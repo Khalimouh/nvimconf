1bab9ac660456e14530765fe90ea423b2c682ed6
Mohamed Ramdane DEBIANE <m.debiane71@gmail.com>
2025-07-27 22:11:26 +0200

    Add noice

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -123,7 +123,7 @@ return {
                 "shfmt",
                 "scalafmt",
                 "sqlfluff",
-                "terragrunt_fmt"
+                "terragrunt_fmt",
             },
             auto_install = true,
         },
@@ -133,6 +133,73 @@ return {
         ft = "*",
         build = function()
             vim.cmd.GoInstallDeps()
+        end,
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = function(_, opts)
+            opts.presets = {
+                command_palette = {
+                    views = {
+                        cmdline_popup = {
+                            position = {
+                                row = "50%",
+                                col = "50%",
+                            },
+                            size = {
+                                min_width = 60,
+                                width = "auto",
+                                height = "auto",
+                            },
+                        },
+                        popupmenu = {
+                            relative = "editor",
+                            position = {
+                                row = 23,
+                                col = "50%",
+                            },
+                            size = {
+                                width = 60,
+                                height = "auto",
+                                max_height = 15,
+                            },
+                            border = {
+                                style = "rounded",
+                                padding = { 0, 1 },
+                            },
+                            win_options = {
+                                winhighlight = { Normal = "Normal", FloatBorder = "NoiceCmdlinePopupBorder" },
+                            },
+                        },
+                    },
+                },
+            }
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+
+        config = function()
+            local noice = require "noice"
+
+            noice.setup {
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            }
         end,
     },
 }
